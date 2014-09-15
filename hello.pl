eb4f702713a05dbaa7251fcd985c9b38656be4ca
Mykal Cuin <mykcuin@gmail.com>
2014-09-15 10:23:51 -0600

    Hello world, and some variable practice.

---
+++ hello.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+print "Hello, World\n";
+print 'Hello, World\n';
+
+$a = 10;
+print "Value of a = $a\n";
+print 'Value of a = $a\n';
+
+$str = "Welcome to \ntutorialspoint.com!";
+print "$str\n";
+
+$str = 'Welcome to \ntutorialspoint.com!';
+print "$str\n";
+
+$str = "\uwelcome to tutorialspoint.com!";
+print "$str\n";
+
+$str = "\Uwelcome to tutorialspoint.com!";
+print "$str\n";
+
+$str = "Welcome to \Ututorialspoint\E.com!";
+print "$str\n";
+
+$str = "\QWelecome to tutorialspoint's family";
+print "$str\n";  
---
+++ variables.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+$age = 25;
+$name = "John Paul";
+$salary = "1445.50";
+
+print "Age = $age\n";
+print "Name = $name\n";
+print "Salary = $salary\n";
+
+@ages = (25, 30, 40);
+@names = ("Jonh Paul", "Lisa", "Kumar");
+
+print "\$ages[0] = $ages[0]\n";
+print "\$ages[1] = $ages[1]\n";
+print "\$ages[2] = $ages[2]\n";
+print "\$names[0] = $names[0]\n";
+print "\$names[1] = $names[1]\n";
+print "\$names[2] = $names[2]\n";
+
+%data = ('John Paul', 45, 'Lisa', 30, 'Kumar', 40);
+
+print "\$data{'John Paul'} = $data{'John Paul'}\n";
+print "\$data{'Lisa'} = $data{'Lisa'}\n";
+print "\$data{'Kumar'} = $data{'Kumar'}\n";
