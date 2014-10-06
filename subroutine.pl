5b8ab721127c75facd000b4f9e1465ded0eda524
Mykal Cuin <mykcuin@gmail.com>
2014-10-06 10:13:50 -0600

    Finished with subrotuines.

--- subroutine.pl
+++ subroutine.pl
@@ -16,6 +16,8 @@ sub Average{
 	$average = $sum / $n;
 
 	print "Average for given numbers is: $average\n";
+
+	return $average;
 }
 
 Average(10, 20, 30);
@@ -41,3 +43,37 @@ sub PrintHash{
 %hash = ('name' => 'Tom', 'age' => 19);
 
 PrintHash(%hash);
+
+$num = Average(40, 50, 60);
+
+print "Average for given numbers id: $num\n";	
+
+$string = "Hello world!";
+
+sub PrintHello{
+	my $string;
+	local $string2;
+	$string = "Hello Perl!";
+	$string2 = "Hello Perl!";
+	print "Inside the function PrintHello $string\n";
+}
+
+sub PrintMe{
+	print "Inside the function PrintMe $string2\n";
+}
+
+PrintHello();
+print "Outside the functions: $string\n";
+
+use feature 'state';
+
+sub PrintCount{
+	state $count = 0;
+
+	print "Value of counter is: $count\n";
+	$count++;
+}
+
+for (1..5){
+	PrintCount();
+}
